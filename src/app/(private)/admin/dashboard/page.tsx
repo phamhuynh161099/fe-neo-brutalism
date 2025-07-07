11d47dcb40115e9c94ce3ef13fdd4a46bfbeab33
phamhuynh161099 <101373851+phamhuynh161099@users.noreply.github.com>
2025-07-07 20:05:44 +0700

    Update page.tsx

--- src/app/(private)/admin/dashboard/page.tsx
+++ src/app/(private)/admin/dashboard/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const DashboardPage = () => {
   return (
     <div>
-      <p>Dashboard index 2 3 4 5</p>
+      <p>Dashboard index 2 3 4 5 7 8 9</p>
     </div>
   );
 };
