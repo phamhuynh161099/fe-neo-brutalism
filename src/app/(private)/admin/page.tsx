a5be298a9c5f376625128be93c3ba79b05a81331
phamhuynh161099 <phamhuynh161099@gmail.com>
2025-07-08 14:08:54 +0700

    change something

--- src/app/(private)/admin/page.tsx
+++ src/app/(private)/admin/page.tsx
@@ -5,7 +5,7 @@ function IndexPage() {
     <div className="w-full min-h-[calc(100vh-3rem)] overflow-auto">
       <div className="w-full h-[200px] bg-pink-200 overflow-auto">
         <div className="h-[100px] w-[1900px]">
-          <p>Sakata1301 456</p>
+          <p>Sakata1301 456 77777</p>
         </div>
       </div>
     </div>
